f7c67ea2eff229d548ea434e0a05b474b5706a71
cysmg14 <cys.mg14@nycu.edu.tw>
2025-11-25 14:24:00 +0800

    init commit

--- src/p2.cpp
+++ src/p2.cpp
@@ -2,18 +2,15 @@
 
 #include <iostream>
 using namespace std;
-
-int main() {
-
-    int a, b;
-    cin >> a >> b;
-    int gcd(int a, int b);
+    int gcd(int a, int b)
     {
       if (b == 0)
           return a;
       return gcd(b, a % b);
     }
-
+int main() {
+    int a, b;
+    cin >> a >> b;
     cout << gcd(a, b);
     
     return 0;
